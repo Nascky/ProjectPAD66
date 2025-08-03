298988430425f849c00f24a3eae02a51badbe2fe
Nascky <nasckyapp@gmail.com>
2025-08-03 11:59:05 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,10 @@ cd /home/ubuntu/ProjectPAD66 || exit 1
 echo "📥 Puxando alterações do GitHub..."
 git pull origin main
 
-echo "🔁 Reiniciando processo no PM2..."
-pm2 restart pad66
+echo "🔄 Reiniciando processos no PM2..."
+pm2 restart pad66-app
+pm2 restart pad66-worker
 
 echo "✅ Deploy finalizado com sucesso!"
-pm2 status pad66
+pm2 status pad66-app
+pm2 status pad66-worker
